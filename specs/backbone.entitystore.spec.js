4716bbc4ae160fde49c2dc9a682e5c3e58327b2e
Grayden Smith <grayden@tech-vision.ca>
2013-11-03 10:57:19 -0500

    Fetch model on get
    When get is called on the entity store, fetch the model that matches it.

--- specs/backbone.entitystore.spec.js
+++ specs/backbone.entitystore.spec.js
@@ -3,4 +3,39 @@ describe("Backbone.EntityStore", function () {
   it("should exist", function () {
     expect(Backbone.EntityStore).toBeDefined();
   });
+
+  describe ("when requesting a model", function () {
+    beforeEach(function () {
+      var KiwiModel = Backbone.Model.extend({
+        url: '/api/kiwis'
+      });
+
+      var KiwiCollection = Backbone.Collection.extend({
+        url: '/api/kiwis',
+        model: KiwiModel
+      });
+      this.entityStore = new Backbone.EntityStore({
+        model: KiwiModel,
+        collection: KiwiCollection
+      });
+
+      this.xhr = sinon.useFakeXMLHttpRequest();
+      var requests = this.requests = []
+
+      this.xhr.onCreate = function (xhr) {
+        requests.push(xhr);
+      };
+    });
+
+    afterEach(function () {
+      this.xhr.restore();
+    });
+
+    it("should be able to get a model that is not in the collection using and ajax request", function () {
+      var spy = jasmine.createSpy('callback');
+      this.entityStore.get(1).done(spy);
+      this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1 }');
+      expect(spy).toHaveBeenCalled();
+    });
+  });
 });
--- src/backbone.entitystore.js
+++ src/backbone.entitystore.js
@@ -1,9 +1,14 @@
 (function (Backbone) {
-  var EntityStore = Backbone.EntityStore = function () {
-
+  var EntityStore = Backbone.EntityStore = function (attrs) {
+    this.modelType = attrs.model;
+    this.collectionType = attrs.collection;
   }; 
 
   _.extend(EntityStore.prototype, {
+    get: function (id) {
+      var newModelInstance = new this.modelType({ id: id });
+      return newModelInstance.fetch();
+    }
 
   });
 })(Backbone);
