46877ffe29937c28a2d0cee982fbeced0ec0751d
Grayden Smith <grayden@tech-vision.ca>
2013-11-13 19:31:27 -0500

    Reorganize test cases
    Order test cases in more logical describe blocks.

--- specs/backbone.entitystore.spec.js
+++ specs/backbone.entitystore.spec.js
@@ -4,7 +4,7 @@ describe("Backbone.EntityStore", function () {
     expect(Backbone.EntityStore).toBeDefined();
   });
 
-  describe ("when requesting a model", function () {
+  describe ("requesting a model", function () {
     beforeEach(function () {
       var KiwiModel = this.KiwiModel = Backbone.Model.extend({
         url: '/api/kiwis'
@@ -18,45 +18,55 @@ describe("Backbone.EntityStore", function () {
         model: KiwiModel,
         collection: KiwiCollection
       });
-
-      this.xhr = sinon.useFakeXMLHttpRequest();
-      var requests = this.requests = []
-
-      this.xhr.onCreate = function (xhr) {
-        requests.push(xhr);
-      };
     });
 
-    afterEach(function () {
-      this.xhr.restore();
+    describe("from the collection", function () {
+      beforeEach(function () {
+        this.entityStore.collection.add(
+          { id: 1, name: "kiwi" }
+        ) 
+      });
+
+      it("should be able to fetch a model by model", function () {
+        var model = new this.KiwiModel({id : 1});
+        var fetchedModel = this.entityStore.get(model);
+        expect(fetchedModel.get('name')).toBe('kiwi');
+      });
+
     });
 
-    it("should be able to get a model that is not in the collection using and ajax request", function () {
-      var spy = jasmine.createSpy('callback');
-      this.entityStore.get(1).done(spy);
-      this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1 }');
-      expect(spy).toHaveBeenCalled();
-    });
+    describe("from the server", function () {
+      beforeEach(function () {
+        this.xhr = sinon.useFakeXMLHttpRequest();
+        var requests = this.requests = []
+
+        this.xhr.onCreate = function (xhr) {
+          requests.push(xhr);
+        };
+      });
+
+      it("should be able to get a model that is not in the collection using and ajax request", function () {
+        var spy = jasmine.createSpy('callback');
+        this.entityStore.get(1).done(spy);
+        this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1 }');
+        expect(spy).toHaveBeenCalled();
+      });
 
 
-    it("should add a fetched model to the collection so that it doesn't need to be fetched again", function () {
-      this.entityStore.get(1);
-      this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1, "name": "jimmy" }');
+      it("should add a fetched model to the collection so that it doesn't need to be fetched again", function () {
+        this.entityStore.get(1);
+        this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1, "name": "jimmy" }');
 
-      expect(this.entityStore.collection.length).toBe(1);
-      
-      var aKiwi = this.entityStore.get(1);
-      expect(aKiwi.get("name")).toBe("jimmy");
-    });
+        expect(this.entityStore.collection.length).toBe(1);
+        
+        var aKiwi = this.entityStore.get(1);
+        expect(aKiwi.get("name")).toBe("jimmy");
+      });
 
-    it("should be able to fetch a model by model", function () {
-      var model = new this.KiwiModel({id : 1});
-      var spy = jasmine.createSpy("callback");
 
-      var fetchedModel = this.entityStore.get(model);
-      fetchedModel.done(spy);
-      this.requests[0].respond(200, { "Content-type": "application/json" }, '{ "id": 1 }');
-      expect(spy).toHaveBeenCalled();
+      afterEach(function () {
+        this.xhr.restore();
+      });
     });
   });
 });
